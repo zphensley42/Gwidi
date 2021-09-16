20ea106ee053070c47212fa264ad476b260d468b
Zachary Hensley <zphensley42@gmail.com>
2021-09-16 15:26:38 -0500

    Various fixes for faster / better drawing

--- editor_v2.py
+++ editor_v2.py
@@ -72,13 +72,25 @@ class Slot:
         self.playing = False
         self.drawn = False
         self.change_triggered = False
+        self.rect = None
+
+    def set_rect(self, r):
+        self.rect = r
 
     def change(self):
+        if self.change_triggered:
+            return
+
         self.activated = not self.activated
         self.change_triggered = True
 
+        dpg.configure_item(self.rect, fill=self.fill())
+
     def clear_change_trigger(self):
         self.change_triggered = False
+
+    def fill(self):
+        return [0, 255, 0, 255] if self.activated else [255, 255, 255, 255]
 
 
 class Note:
@@ -158,6 +170,36 @@ class ControlBar:
             self.load = dpg.add_button(label="Load", callback=self.cb_load, pos=[120, 0], height=self.height(), width=50)
 
 
+class InfoBar:
+    def __init__(self):
+        self.data_label = 'Debug_Info: '
+        self.mouse_pos = []
+        self.translated_mouse_pos = []
+        self.data_text = None
+
+    def refresh(self):
+        dpg.configure_item(self.data_text, default_value=self.data_string())
+
+    def set_mouse_pos(self, mouse_pos):
+        self.mouse_pos = mouse_pos
+        self.refresh()
+
+    def set_translated_mouse_pos(self, mouse_pos):
+        self.translated_mouse_pos = mouse_pos
+        self.refresh()
+
+    def data_string(self):
+        return "{label} mouse_pos: {mp} translated: {mp2}".format(
+            label=self.data_label,
+            mp=self.mouse_pos,
+            mp2=self.translated_mouse_pos,
+        )
+
+    def draw(self):
+        global main_window
+        self.data_text = dpg.add_text(parent=Elements.dpg_window, pos=[10, 65], default_value=self.data_string())
+
+
 class MeasuresDisplay:
     def __init__(self):
         self.measures_panel = None
@@ -172,24 +214,39 @@ class MeasuresDisplay:
             dpg.delete_item(self.measures_panel)
             self.measures_panel = None
 
+    def content_width(self):
+        # oct_width = num slots * slot width
+        # full width = num measures * oct width
+        oct_w = len(self.measures[0].octaves[0].notes[0].slots) * MeasureDisplay.slot_width
+        return len(self.measures) * (oct_w + MeasureDisplay.measure_spacing)
+
+    def content_height(self):
+        # oct_height = num notes * note slot height
+        # measure height = num octaves * oct height
+        oct_h = len(self.measures[0].octaves[0].notes) * MeasureDisplay.slot_height
+        return len(self.measures[0].octaves) * (oct_h + MeasureDisplay.octave_spacing)
+
     def draw(self):
         # self.clear()
 
-        with dpg.child(parent=Elements.dpg_window, horizontal_scrollbar=True, autosize_y=True, autosize_x=True, pos=[0, 50]) as p:
+        with dpg.child(parent=Elements.dpg_window, horizontal_scrollbar=True, autosize_y=True, autosize_x=True, pos=[0, 100]) as p:
             self.measures_panel = p
+            with dpg.drawlist(width=self.content_width(), height=self.content_height() + 50) as dl:
+                for iteration, m in enumerate(self.measures):
+                    md = MeasureDisplay()
+                    md.set_data(m, iteration)
+                    self.measure_displays.append(md)
 
-        for iteration, m in enumerate(self.measures):
-            md = MeasureDisplay()
-            md.set_data(m, iteration)
-            self.measure_displays.append(md)
-
-            md.draw(self.measures_panel)
+                    md.draw(dl)
 
 
 class MeasureDisplay:
     slot_width = 30
     slot_height = 20
     slot_spacing = 4
+
+    octave_spacing = 20
+    measure_spacing = 10
 
     def __init__(self):
         self.measure = None
@@ -202,93 +259,77 @@ class MeasureDisplay:
         self.measure = d
         self.index = i
 
-    def octave_height(self):
+    def measure_width(self):
+        # width of measure is # of slots * slot width
+        return len(self.measure.octaves[0].notes[0].slots) * MeasureDisplay.slot_width
+
+    def measure_height(self):
+        # height of measure is # of notes * slot_height
         return len(self.measure.octaves[0].notes) * MeasureDisplay.slot_height
 
-    def height(self):
-        # num of notes * height of slot = height of octave
-        # height of octave * num of octaves = height of measure
-        return len(self.measure.octaves) * (self.octave_height() + 40)
+    def draw_octave(self, parent, measure_ind, octave_ind):
+        oct = self.measure.octaves[octave_ind]
 
-    def width(self):
-        num_slots = len(self.measure.octaves[0].notes[0].slots)
-        return MeasureDisplay.slot_width * num_slots
+        # measure_ind == x offset
+        # octave_ind == y offset
+        x_off = measure_ind * (self.measure_width() + MeasureDisplay.measure_spacing)
+        y_off = octave_ind * (self.measure_height() + MeasureDisplay.octave_spacing)
+
+        # for each octave, draw the associated rectangles for the notes / slots
+        for note_iter, note in enumerate(oct.notes):
+            for slot_iter, slot in enumerate(note.slots):
+                start_x = x_off + slot_iter * MeasureDisplay.slot_width
+                start_y = y_off + note_iter * MeasureDisplay.slot_height
+                r = dpg.draw_rectangle(
+                    pmin=[start_x + (MeasureDisplay.slot_spacing / 2), start_y + (MeasureDisplay.slot_spacing / 2)],
+                    pmax=[start_x + MeasureDisplay.slot_width - (MeasureDisplay.slot_spacing / 2),
+                          start_y + MeasureDisplay.slot_height - (MeasureDisplay.slot_spacing / 2)],
+                    fill=slot.fill()
+                )
+                slot.set_rect(r)
+
+    def delegate_mouse_down(self, pos):
+        global main_window
+
+        # Things to care about when determining what rect we are in:
+        # 'window' position (top-left corner) on the screen: dpg.get_viewport_pos()
+        # scroll offset of the measures display child
+
+        # self.index is the index of the measure we are
+        slot_found = None
+        for oct_ind, oct in enumerate(self.measure.octaves):
+            if slot_found is not None:
+                break
+
+            x_off = self.index * (self.measure_width() + MeasureDisplay.measure_spacing)
+            y_off = oct_ind * (self.measure_height() + MeasureDisplay.octave_spacing)
+
+            for note_iter, note in enumerate(oct.notes):
+                if slot_found is not None:
+                    break
+                for slot_iter, slot in enumerate(note.slots):
+
+                    start_x = x_off + slot_iter * MeasureDisplay.slot_width
+                    start_y = y_off + note_iter * MeasureDisplay.slot_height
+
+                    in_x = start_x <= pos[0] <= start_x + MeasureDisplay.slot_width
+                    in_y = start_y <= pos[1] <= start_y + MeasureDisplay.slot_height
+
+                    if in_x and in_y:
+                        slot_found = slot
+                        break
+
+        print('slot_found: {s}'.format(s=str(slot_found)))
+        if slot_found is not None:
+            slot_found.change()
 
     def draw(self, parent):
         print('drawing measure display')
 
-        with dpg.child(parent=parent, width=self.width() + 40, height=self.height(), pos=[(self.width() + 40) * self.index, 0]) as m:
-            self.measure_panel = m
-
-            # draw 3 octaves
-            oct0 = self.measure.octaves[0]
-            with dpg.child(parent=m, height=self.octave_height() + 20, width=self.width() + 20) as op:
-                with dpg.drawlist(width=self.width(), height=self.octave_height()) as dl:
-
-                    # for each octave, draw the associated rectangles for the notes / slots
-                    for note_iter, note in enumerate(oct0.notes):
-                        for slot_iter, slot in enumerate(note.slots):
-                            start_x = slot_iter * MeasureDisplay.slot_width
-                            start_y = note_iter * MeasureDisplay.slot_height
-                            dpg.draw_rectangle(
-                                pmin=[start_x + (MeasureDisplay.slot_spacing / 2), start_y + (MeasureDisplay.slot_spacing / 2)],
-                                pmax=[start_x + MeasureDisplay.slot_width - (MeasureDisplay.slot_spacing / 2), start_y + MeasureDisplay.slot_height - (MeasureDisplay.slot_spacing / 2)],
-                                fill=[255, 255, 255, 255]
-                            )
-
-                with dpg.theme() as theme:
-                    dpg.add_theme_color(target=dpg.mvThemeCol_ChildBg, value=[0, 0, 0, 0], category=dpg.mvThemeCat_Core)
-                    dpg.add_theme_color(target=dpg.mvThemeCol_Border, value=[0, 0, 0, 0], category=dpg.mvThemeCat_Core)
-
-                    dpg.set_item_theme(item=op, theme=theme)
-
-                self.octave_panels.append(op)
-
-            oct1 = self.measure.octaves[1]
-            with dpg.child(parent=m, height=self.octave_height() + 20, width=self.width() + 20) as op:
-                with dpg.drawlist(width=self.width(), height=self.octave_height()) as dl:
-
-                    # for each octave, draw the associated rectangles for the notes / slots
-                    for note_iter, note in enumerate(oct1.notes):
-                        for slot_iter, slot in enumerate(note.slots):
-                            start_x = slot_iter * MeasureDisplay.slot_width
-                            start_y = note_iter * MeasureDisplay.slot_height
-                            dpg.draw_rectangle(
-                                pmin=[start_x + (MeasureDisplay.slot_spacing / 2), start_y + (MeasureDisplay.slot_spacing / 2)],
-                                pmax=[start_x + MeasureDisplay.slot_width - (MeasureDisplay.slot_spacing / 2), start_y + MeasureDisplay.slot_height - (MeasureDisplay.slot_spacing / 2)],
-                                fill=[255, 255, 255, 255]
-                            )
-
-                with dpg.theme() as theme:
-                    dpg.add_theme_color(target=dpg.mvThemeCol_ChildBg, value=[0, 0, 0, 0], category=dpg.mvThemeCat_Core)
-                    dpg.add_theme_color(target=dpg.mvThemeCol_Border, value=[0, 0, 0, 0], category=dpg.mvThemeCat_Core)
-
-                    dpg.set_item_theme(item=op, theme=theme)
-
-                self.octave_panels.append(op)
-
-            oct2 = self.measure.octaves[2]
-            with dpg.child(parent=m, height=self.octave_height() + 20, width=self.width() + 20) as op:
-                with dpg.drawlist(width=self.width(), height=self.octave_height()) as dl:
-
-                    # for each octave, draw the associated rectangles for the notes / slots
-                    for note_iter, note in enumerate(oct2.notes):
-                        for slot_iter, slot in enumerate(note.slots):
-                            start_x = slot_iter * MeasureDisplay.slot_width
-                            start_y = note_iter * MeasureDisplay.slot_height
-                            dpg.draw_rectangle(
-                                pmin=[start_x + (MeasureDisplay.slot_spacing / 2), start_y + (MeasureDisplay.slot_spacing / 2)],
-                                pmax=[start_x + MeasureDisplay.slot_width - (MeasureDisplay.slot_spacing / 2), start_y + MeasureDisplay.slot_height - (MeasureDisplay.slot_spacing / 2)],
-                                fill=[255, 255, 255, 255]
-                            )
-
-                with dpg.theme() as theme:
-                    dpg.add_theme_color(target=dpg.mvThemeCol_ChildBg, value=[0, 0, 0, 0], category=dpg.mvThemeCat_Core)
-                    dpg.add_theme_color(target=dpg.mvThemeCol_Border, value=[0, 0, 0, 0], category=dpg.mvThemeCat_Core)
-
-                    dpg.set_item_theme(item=op, theme=theme)
-
-                self.octave_panels.append(op)
+        # draw 3 octaves
+        self.draw_octave(parent, self.index, 0)
+        self.draw_octave(parent, self.index, 1)
+        self.draw_octave(parent, self.index, 2)
 
 
 # TODO: Don't need the panels (other than the main panel used to hold MeasuresDisplay probably)
@@ -299,11 +340,13 @@ class MainWindow:
         global g_measures
 
         self.controls = ControlBar()
+        self.info = InfoBar()
         self.measures = MeasuresDisplay()
         self.measures.set_data(g_measures)
 
     def draw(self):
         self.controls.draw()
+        self.info.draw()
         self.measures.draw()
 
 
@@ -330,15 +373,22 @@ class MouseStats:
 
     def moved(self, sender, app_data):
         global g_measures
+        global main_window
 
         self.pos = app_data
         print('moved: {pos}, down: {d}'.format(pos=self.pos, d=self.is_down))
 
-        # if self.is_down:
-        #     for m in g_measures:
-        #         for o in m.octaves:
-        #             for n in o.notes:
-        #                 for s in n.slots:
+        translated_mouse_pos = [
+            self.pos[0] + dpg.get_x_scroll(main_window.measures.measures_panel),
+            self.pos[1] + dpg.get_y_scroll(main_window.measures.measures_panel) - dpg.get_item_pos(main_window.measures.measures_panel)[1],
+        ]
+
+        main_window.info.set_mouse_pos(self.pos)
+        main_window.info.set_translated_mouse_pos(translated_mouse_pos)
+
+        if self.is_down:
+            for mdisplay in main_window.measures.measure_displays:
+                mdisplay.delegate_mouse_down(translated_mouse_pos)
 
 
 
@@ -355,6 +405,13 @@ class MouseStats:
 
         print('upped')
         self.is_down = False
+
+        # reset trigger states for all slots
+        for m in g_measures:
+            for o in m.octaves:
+                for n in o.notes:
+                    for s in n.slots:
+                        s.clear_change_trigger()
 
 
 ms = MouseStats()
