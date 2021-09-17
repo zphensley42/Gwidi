3fca1643f364045a6c3a20a325695167dcf1215a
Zachary Hensley <zphensley42@gmail.com>
2021-09-17 10:25:58 -0500

    Much imporved v2, TODOs listed in source

--- editor_v2.py
+++ editor_v2.py
@@ -10,9 +10,16 @@ import threading
 pydirectinput.PAUSE = 0
 
 
+# TODO: Drawn / Loading states for when we are waiting for things to finish drawing
+# TODO: List of songs / files for save/load
+# TODO: Macros
+# TODO: left click add, right click remove
+# TODO: difference between 'strung' notes and 'held' notes so that the tabs can look closer to the MIDI
+
+
 class Constants:
-    vp_width = 800
-    vp_height = 600
+    vp_width = 1000
+    vp_height = 800
 
     slots_per_measure = 16
     measures_count = 3
@@ -71,6 +78,9 @@ class Elements:
 
 # TODO: Draw the note labels on the left of the total window (keep the offset for mouse positioning likely)
 
+# TODO: Some timing issues with clear / load / etc freezing the UI (and sleeps for playback doing similar when going between octaves?)
+# TODO: Perhaps make restriction on how notes work across octaves (i.e. no multiple notes at the same time across multiple octaves)
+
 
 class Slot:
 
@@ -80,6 +90,7 @@ class Slot:
         self.drawn = False
         self.change_triggered = False
         self.rect = None
+        self.rect_text = None
         self.note_key = None
         self.note_octave = None
 
@@ -90,6 +101,9 @@ class Slot:
     def set_rect(self, r):
         self.rect = r
 
+    def set_rect_text(self, r):
+        self.rect_text = r
+
     def change(self):
         if self.change_triggered:
             return
@@ -98,22 +112,39 @@ class Slot:
         self.change_triggered = True
 
         dpg.configure_item(self.rect, fill=self.fill())
+        dpg.configure_item(self.rect_text, show=self.activated)
+
+    def clear(self):
+        self.activated = False
+        self.change_triggered = False
+        self.playing = False
+
+        dpg.configure_item(self.rect, fill=self.fill())
+        dpg.configure_item(self.rect_text, show=self.activated)
+        dpg.configure_item(self.rect, color=self.color())
+
 
     def clear_change_trigger(self):
         self.change_triggered = False
 
     def send_inputs(self):
-        if self.note_octave == 0:  # high
-            pydirectinput.press('9')
-        elif self.note_octave == 2:  # low
-            pydirectinput.press('0')
-
         pydirectinput.press(self.note_key['key'])
 
-        if self.note_octave == 0:  # high
-            pydirectinput.press('0')
-        elif self.note_octave == 2:  # low
-            pydirectinput.press('9')
+        # if self.note_octave == 0:  # high
+        #     pydirectinput.press('9')
+        #     time.sleep(0.1)
+        # elif self.note_octave == 2:  # low
+        #     pydirectinput.press('0')
+        #     time.sleep(0.1)
+        #
+        # pydirectinput.press(self.note_key['key'])
+        #
+        # if self.note_octave == 0:  # high
+        #     pydirectinput.press('0')
+        #     time.sleep(0.1)
+        # elif self.note_octave == 2:  # low
+        #     pydirectinput.press('9')
+        #     time.sleep(0.1)
 
     def play(self):
         self.playing = True
@@ -173,10 +204,13 @@ class ControlBar:
     def __init__(self):
         self.control_bar = None
         self.play_pause = None
+        self.clear_but = None
         self.save = None
         self.load = None
         self.measure_cnt_ctrl = None
         self.measure_cnt_ctrl_apply = None
+        self.bpm_ctrl = None
+        self.bpm_ctrl_apply = None
 
     def clear(self):
         if self.control_bar is not None:
@@ -196,10 +230,13 @@ class ControlBar:
         toggled = not toggled
         dpg.set_item_user_data(self.play_pause, toggled)
 
-        l = "Play" if not toggled else "Pause"
+        l = "Play" if not toggled else "Stop"
         dpg.set_item_label(self.play_pause, label=l)
 
-        start_timer()
+        if toggled:
+            start_timer()
+        else:
+            stop_timer()
 
     def cb_cnt_changed(self, sender, app_data):
         dpg.set_item_user_data(sender, app_data)
@@ -208,13 +245,36 @@ class ControlBar:
         cnt = dpg.get_item_user_data(self.measure_cnt_ctrl)
         Constants.measures_count = cnt
         init_measures(cnt)
+        main_window.refresh_measures()
         main_window.draw()
 
     def cb_save(self, sender, app_data):
         print('cb_save')
+        save('test_save.out')
 
     def cb_load(self, sender, app_data):
         print('cb_load')
+        load('test.out')
+
+    def cb_bpm_changed(self, sender, app_data):
+        dpg.set_item_user_data(sender, app_data)
+
+    def cb_apply_bpm(self, sender, app_data):
+        bpm = dpg.get_item_user_data(self.bpm_ctrl)
+        TimeManager.BPM = bpm
+
+        global g_tm
+        if g_tm is not None and g_tm.alive:
+            g_tm.kill()
+
+    def cb_clear(self, sender, app_data):
+        dpg.set_item_user_data(self.play_pause, False)
+        dpg.set_item_label(self.play_pause, label="Play")
+
+        stop_timer()
+        clear_measures()
+        main_window.refresh_measures()
+        main_window.draw()
 
     def draw(self):
         # self.clear()
@@ -224,12 +284,18 @@ class ControlBar:
             self.play_pause = dpg.add_button(label="Play", callback=self.cb_play, pos=[0, 0], height=self.height(), width=50)
             dpg.set_item_user_data(self.play_pause, False)
 
-            self.save = dpg.add_button(label="Save", callback=self.cb_save, pos=[60, 0], height=self.height(), width=50)
-            self.load = dpg.add_button(label="Load", callback=self.cb_load, pos=[120, 0], height=self.height(), width=50)
+            self.clear_but = dpg.add_button(label="Clear", callback=self.cb_clear, pos=[60, 0], height=self.height(), width=50)
 
-            self.measure_cnt_ctrl = dpg.add_input_int(label="# of Measures", callback=self.cb_cnt_changed, pos=[200, 20], default_value=Constants.measures_count, width=100)
-            self.measure_cnt_ctrl_apply = dpg.add_button(label="Apply", callback=self.cb_apply, pos=[410, 0], height=self.height(), width=50)
+            self.save = dpg.add_button(label="Save", callback=self.cb_save, pos=[120, 0], height=self.height(), width=50)
+            self.load = dpg.add_button(label="Load", callback=self.cb_load, pos=[180, 0], height=self.height(), width=50)
+
+            self.measure_cnt_ctrl = dpg.add_input_int(label="# of Measures", callback=self.cb_cnt_changed, pos=[260, 20], default_value=Constants.measures_count, width=100)
+            self.measure_cnt_ctrl_apply = dpg.add_button(label="Apply", callback=self.cb_apply, pos=[470, 0], height=self.height(), width=50)
             dpg.set_item_user_data(self.measure_cnt_ctrl, Constants.measures_count)
+
+            self.bpm_ctrl = dpg.add_input_int(label="BPM", callback=self.cb_bpm_changed, pos=[540, 20], default_value=TimeManager.BPM, max_value=999, width=100)
+            self.bpm_ctrl_apply = dpg.add_button(label="Apply", callback=self.cb_apply_bpm, pos=[680, 0], height=self.height(), width=50)
+            dpg.set_item_user_data(self.bpm_ctrl, TimeManager.BPM)
 
 
 class InfoBar:
@@ -269,6 +335,7 @@ class InfoBar:
 
 
 class MeasuresDisplay:
+    drawlist_offset = 30
     def __init__(self):
         self.measures_panel = None
         self.measures = []
@@ -299,7 +366,7 @@ class MeasuresDisplay:
 
         with dpg.child(parent=Elements.dpg_window, horizontal_scrollbar=True, autosize_y=True, autosize_x=True, pos=[0, 100]) as p:
             self.measures_panel = p
-            with dpg.drawlist(width=self.content_width(), height=self.content_height() + 50) as dl:
+            with dpg.drawlist(pos=[MeasuresDisplay.drawlist_offset, 0], width=self.content_width(), height=self.content_height() + 50) as dl:
                 for iteration, m in enumerate(self.measures):
                     md = MeasureDisplay()
                     md.set_data(m, iteration)
@@ -346,6 +413,9 @@ class MeasureDisplay:
         # for each octave, draw the associated rectangles for the notes / slots
         for note_iter, note in enumerate(oct.notes):
             for slot_iter, slot in enumerate(note.slots):
+
+                draw_quarter_bar = (slot_iter % 4) == 0
+
                 start_x = x_off + slot_iter * MeasureDisplay.slot_width
                 start_y = y_off + note_iter * MeasureDisplay.slot_height
                 r = dpg.draw_rectangle(
@@ -356,7 +426,18 @@ class MeasureDisplay:
                     color=slot.color(),
                     thickness=4,
                 )
+                print('drawing text: ' + slot.note_key['label'])
+                t = 'o{ov}{nk}'.format(ov=slot.note_octave, nk=slot.note_key['label'])
+                rect_text = dpg.draw_text(size=11, show=slot.activated, color=[0, 0, 0, 255], text=t, pos=[start_x + (MeasureDisplay.slot_spacing / 2) + 1, start_y + (MeasureDisplay.slot_spacing / 2) + 2])
                 slot.set_rect(r)
+                slot.set_rect_text(rect_text)
+
+                if draw_quarter_bar:
+                    sx = start_x + (MeasureDisplay.slot_spacing / 2)
+                    sy = start_y + (MeasureDisplay.slot_spacing / 2)
+                    ex = sx
+                    ey = start_y + MeasureDisplay.slot_height - (MeasureDisplay.slot_spacing / 2)
+                    dpg.draw_line(p1=[sx, sy], p2=[ex, ey], thickness=1, color=[255, 0, 0, 255])
 
     def delegate_mouse_down(self, pos):
         global main_window
@@ -371,7 +452,7 @@ class MeasureDisplay:
             if slot_found is not None:
                 break
 
-            x_off = self.index * (self.measure_width() + MeasureDisplay.measure_spacing)
+            x_off = self.index * (self.measure_width() + MeasureDisplay.measure_spacing) + MeasuresDisplay.drawlist_offset
             y_off = oct_ind * (self.measure_height() + MeasureDisplay.octave_spacing)
 
             for note_iter, note in enumerate(oct.notes):
@@ -396,9 +477,24 @@ class MeasureDisplay:
         print('drawing measure display')
 
         # draw 3 octaves
-        self.draw_octave(parent, self.index, 0)
-        self.draw_octave(parent, self.index, 1)
         self.draw_octave(parent, self.index, 2)
+        self.draw_octave(parent, self.index, 1)
+        self.draw_octave(parent, self.index, 0)
+
+
+class NoteLabels:
+
+    def draw(self):
+        h = dpg.get_item_height(Elements.dpg_window)
+        with dpg.drawlist(parent=main_window.measures.measures_panel, height=h, width=50, pos=[10, 0]) as dl:
+            for i in range(3):
+                # per octave
+                for nl_iter, nl in enumerate(Constants.notes):
+
+                    y_off = i * (MeasureDisplay.octave_spacing + (MeasureDisplay.slot_height * len(Constants.notes))) + (nl_iter * MeasureDisplay.slot_height)
+
+                    dpg.draw_text(size=12, text=nl['label'], pos=[0, y_off])
+
 
 
 # TODO: Don't need the panels (other than the main panel used to hold MeasuresDisplay probably)
@@ -406,15 +502,23 @@ class MeasureDisplay:
 class MainWindow:
 
     def __init__(self):
+        self.controls = None
+        self.info = None
+        self.measures = None
+        self.note_labels = None
+        self.refresh_measures()
+
+    def clear(self):
+        dpg.delete_item(Elements.dpg_window, children_only=True)
+
+    def refresh_measures(self):
         global g_measures
 
         self.controls = ControlBar()
         self.info = InfoBar()
         self.measures = MeasuresDisplay()
         self.measures.set_data(g_measures)
-
-    def clear(self):
-        dpg.delete_item(Elements.dpg_window, children_only=True)
+        self.note_labels = NoteLabels()
 
     def draw(self):
         self.clear()
@@ -422,6 +526,7 @@ class MainWindow:
         self.controls.draw()
         self.info.draw()
         self.measures.draw()
+        self.note_labels.draw()
 
 
 def on_viewport_resize():
@@ -440,6 +545,131 @@ def init_measures(count):
     for i in range(count):
         g_measures.append(Measure())
 
+def clear_measures():
+    for m in g_measures:
+        for o in m.octaves:
+            for n in o.notes:
+                for s in n.slots:
+                    s.clear()
+
+def save(fname):
+    global g_measures
+
+    # 130[o0n1-o0n5-o1n3-o2n4]
+    # BPM[<--> note info]
+    #    [octave+note-octave+note-etc]
+
+    f = open(fname, 'w')
+    f.write(str(TimeManager.BPM))
+
+    # TODO: Test case for [] meaning empty slots for that index
+    for m in g_measures:
+
+        for slot_index in range(len(m.octaves[0].notes[0].slots)):
+            f.write('[')
+            write_delim = False
+            for o in m.octaves:
+                for n in o.notes:
+                    slot = n.slots[slot_index]
+
+                    if slot.activated:
+                        if write_delim:
+                            f.write('-')
+
+                        f.write('o{ov}n{nv}'.format(ov=slot.note_octave, nv=slot.note_key['key']))
+                        write_delim = True
+
+            f.write(']')
+
+
+def load(fname):
+    global g_measures
+    global main_window
+    clear_measures()
+
+    f = open(fname, 'r')
+    song_info = f.read()
+
+    # 130[o0n1-o0n5-o1n3-o2n4]
+    # BPM[<--> note info]
+    #    [octave+note-octave+note-etc]
+
+    # parse the info
+    first_index = song_info.index('[')
+    bpm = song_info[0:first_index]
+    notes = song_info[first_index:]
+
+    print('load, bpm: ' + bpm)
+    print('load, notes: ' + notes)
+
+    parsed_measures = [Measure()]
+    measure_index = 0
+    slot_index = 0
+
+    while len(notes) > 0:
+        print('len: ' + str(len(notes)))
+
+        range_begin = notes.index('[')
+        range_end = notes.index(']')
+        range = notes[range_begin+1:range_end]
+
+        print('parse info: {rb} {re} {r}'.format(rb=range_begin, re=range_end, r=range))
+
+        # do something with the info
+        if slot_index >= Constants.slots_per_measure:
+            measure_index += 1
+            slot_index = 0
+            parsed_measures.append(Measure())
+            print('new measure')
+
+        if range_end == (range_begin + 1):
+            notes = notes[range_end + 1:]
+            print('new notes: ' + notes)
+
+            slot_index += 1
+            continue
+
+        # TODO: Sanitization of parsed values instead of just crashing for bad indices
+        octave_notes = range.split('-')
+        for on in octave_notes:
+            # first 2 chars are the octave indicator
+            oct_ind = on[0:2]
+            # second 2 chars are the note indicator
+            note_ind = on[2:4]
+
+            print('oct_ind: {oi}, note_ind: {ni}'.format(oi=oct_ind, ni=note_ind))
+
+            oct_val = int(oct_ind[1])
+            note_val = note_ind[1]
+
+            print('oct_val: {ov}, note_val: {nv}'.format(ov=oct_val, nv=note_val))
+            print('parsed_measures: ' + str(parsed_measures))
+
+            selected_oct = None
+            for o in parsed_measures[measure_index].octaves:
+                if o.octave_val == oct_val:
+                    selected_oct = o
+                    break
+            if selected_oct is not None:
+                for n in selected_oct.notes:
+                    if n.note['key'] == note_val:
+                        n.slots[slot_index].activated = True
+                        break
+
+        notes = notes[range_end+1:]
+        print('new notes: ' + notes)
+
+        slot_index += 1
+
+    # after building our parsed measures, replace our g_measures and draw
+    Constants.measures_count = len(parsed_measures)
+    TimeManager.BPM = int(bpm)
+
+    g_measures = parsed_measures
+    print_measures()
+    main_window.refresh_measures()
+    main_window.draw()
+
 
 class MouseStats:
     is_down = False
@@ -450,7 +680,7 @@ class MouseStats:
         global main_window
 
         self.pos = app_data
-        print('moved: {pos}, down: {d}'.format(pos=self.pos, d=self.is_down))
+        # print('moved: {pos}, down: {d}'.format(pos=self.pos, d=self.is_down))
 
         translated_mouse_pos = [
             self.pos[0] + dpg.get_x_scroll(main_window.measures.measures_panel),
@@ -555,6 +785,7 @@ class TimeManager(threading.Thread):
         self.finished_cb = finished_cb
 
     def run(self):
+        time.sleep(1)   # initial play delay
         while self.alive:
             time.sleep(self.speed)
             self.tick()
@@ -572,10 +803,12 @@ class TimeManager(threading.Thread):
         # self.join()
 
 
-play_time = 0
+play_time = -1
 last_tick_slots = []
+last_octave = 1
 def time_tick():
     global last_tick_slots
+    global last_octave
     global play_time
     global g_measures
     global g_tm
@@ -606,23 +839,54 @@ def time_tick():
 
     main_window.info.set_play_index('play_time: {pt} measure_index: {mi} slot_index: {si}'.format(pt=play_time, mi=measure_index, si=slot_index))
 
-    for n in o0.notes:
+    # TODO: When playing notes, only 'switch' octave as necessary (i.e. we detect that the next note is on a different octave, don't switch back until we need to)
+
+    # determine which octave has notes to play
+    prev_last_octave = last_octave
+    oct_found = None
+    for o in m.octaves:
+        if oct_found:
+            break
+        for n in o.notes:
+            s = n.slots[slot_index]
+            if s.activated:
+                last_octave = o.octave_val
+                oct_found = o
+                break
+
+    # default if not found
+    if oct_found is None:
+        last_octave = 1
+        oct_found = m.octaves[1]
+
+    print('last_octave: {lo}   prev_last_octave: {plo}'.format(lo=last_octave, plo=prev_last_octave))
+
+    # 'switch' octaves as necessary
+    if last_octave > prev_last_octave:
+        for i in range(last_octave - prev_last_octave):
+            pydirectinput.press('0')  # move up
+            time.sleep(0.1)
+    elif last_octave < prev_last_octave:
+        for i in range(prev_last_octave - last_octave):
+            pydirectinput.press('9')  # move down
+            time.sleep(0.1)
+
+    # 'play' them all
+    for n in oct_found.notes:
         s = n.slots[slot_index]
         s.play()
         last_tick_slots.append(s)
-    for n in o1.notes:
-        s = n.slots[slot_index]
-        s.play()
-        last_tick_slots.append(s)
-    for n in o2.notes:
-        s = n.slots[slot_index]
-        s.play()
-        last_tick_slots.append(s)
+
+    # assign our scroll
+    scroll_width = (main_window.measures.measure_displays[0].measure_width() * measure_index) + (slot_index * MeasureDisplay.slot_width)
+    print('scroll_width: ' + str(scroll_width) + '   mi: ' + str(measure_index) + '   si: ' + str(slot_index))
+    dpg.set_x_scroll(main_window.measures.measures_panel, scroll_width)
 
 
 def time_finished():
     dpg.set_item_user_data(main_window.controls.play_pause, False)
     dpg.set_item_label(main_window.controls.play_pause, "Play")
+    dpg.set_x_scroll(main_window.measures.measures_panel, 0)
 
 
 g_tm = None
@@ -630,11 +894,30 @@ def start_timer():
     global g_tm
     global play_time
     if g_tm is None or not g_tm.alive:
-        play_time = 0
+        play_time = -1
         g_tm = TimeManager()
         g_tm.set_cb(time_tick, time_finished)
         g_tm.start()
 
+def stop_timer():
+    global g_tm
+    global play_time
+    if g_tm is not None:
+        g_tm.kill()
+        # g_tm.join()
+        play_time = -1
+
+
+def print_measures():
+    for m in g_measures:
+        for o in m.octaves:
+            print('octave' + str(o.octave_val))
+            for n in o.notes:
+                print('note' + str(n.note))
+                for s in n.slots:
+                    print('slot' + str(s.activated))
+
+    print('measures: ' + str(g_measures))
 
 
 if __name__ == '__main__':
